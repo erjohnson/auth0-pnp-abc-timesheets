8d0167dbc06270ad095e27379d75f7729fc3dd18
erjohnson <eric.johnson@auth0.com>
2017-06-26 13:44:01 -0700

    Add TimeSheets class and layout

---
+++ timesheets-mobile/android/src/app/src/main/java/com/auth0/samples/TimeSheets.java
@@ -0,0 +1,27 @@
+package com.auth0.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by ej on 6/26/17.
+ */
+
+public class TimeSheets extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_sheets);
+    }
+
+    private void getEntries() {
+
+    }
+
+    private void createEntry() {
+        
+    }
+}
